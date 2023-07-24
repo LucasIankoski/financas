403625439a9a364fd292b3fc2245d4d9b10dff90
Lucas Iankoski <lucas@digihub.com.br>
2023-07-24 10:51:31 -0300

    Commit de configurações iniciais.

---
+++ src/test/java/com/iankoski/financas/FinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iankoski.financas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
