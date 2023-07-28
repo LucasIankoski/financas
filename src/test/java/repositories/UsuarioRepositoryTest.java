8db7cdbb7456b3b198343db1f8f0357fa050a737
Lucas Iankoski <lucas@digihub.com.br>
2023-07-27 21:19:13 -0300

    test: atualizando a versão do jUnit

--- src/test/java/repositories/UsuarioRepositoryTest.java
+++ src/test/java/repositories/UsuarioRepositoryTest.java
@@ -2,14 +2,15 @@ package repositories;
 
 import entities.Usuario;
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 public class UsuarioRepositoryTest {
 
     @Autowired
