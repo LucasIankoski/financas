3941e5f6849279f9bb9b2c75fddb6054d391b0e8
Lucas Iankoski <lucas@digihub.com.br>
2023-07-28 10:52:28 -0300

    test: Validando se retorna false na verificação do e-mail.

--- src/test/java/repositories/UsuarioRepositoryTest.java
+++ src/test/java/repositories/UsuarioRepositoryTest.java
@@ -6,11 +6,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
     @Autowired
@@ -24,5 +27,15 @@ public class UsuarioRepositoryTest {
         boolean resultado = repository.existsByEmail("lucas.iankoski@outlook.com");
 
         Assertions.assertThat(resultado).isTrue();
+
+    }
+
+    @Test
+    public void deveRetornarFalsoSeEmailNaoExisteNaBase(){
+        repository.deleteAll();
+
+        boolean resultado = repository.existsByEmail("lucas.iankoski@outlook.com");
+
+        Assertions.assertThat(resultado).isFalse();
     }
 }
