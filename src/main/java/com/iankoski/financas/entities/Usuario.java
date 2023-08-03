d8774766ebffebe50d08360b4e6d5d8873b01636
Lucas Iankoski <lucas@digihub.com.br>
2023-08-02 22:02:09 -0300

    fix: Adicionando as anotações para o construtor default da entidade usuário.

--- src/main/java/com/iankoski/financas/entities/Usuario.java
+++ src/main/java/com/iankoski/financas/entities/Usuario.java
@@ -9,6 +9,8 @@ import java.util.Objects;
 @Data
 @Table(name = "usuario", schema = "mfinancas")
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario {
 
     @Id
@@ -25,36 +27,5 @@ public class Usuario {
     @Column(name = "senha")
     private String senha;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 
 }
